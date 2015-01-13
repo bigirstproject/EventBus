7612a760eb77bc2f93e2a331eda892914989fca5
sunsun <fengyinandong@163.com>
2015-01-13 18:03:03 +0800

    first submit
    Signed-off-by: sunsun <fengyinandong@163.com>

---
+++ src/com/duowan/eventbus/ui/MainActivity.java
@@ -0,0 +1,17 @@
+package com.duowan.eventbus.ui;
+
+import com.duowan.eventbus.R;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+}
---
+++ src/com/duowan/greenrobot/util/HasExecutionScope.java
@@ -0,0 +1,8 @@
+package com.duowan.greenrobot.util;
+
+public interface HasExecutionScope {
+    Object getExecutionScope();
+
+    void setExecutionScope(Object executionScope);
+
+}
